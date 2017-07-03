cdce71513541d3efbc3d00b09afced3dff84fc43
Dhruv Mongia <dhruv.mongia@gmail.com>
2017-07-03 22:06:01 +0530

    fixed users model

--- models/user.js
+++ models/user.js
@@ -2,18 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-    _id: {
-        type: String
-    },
-    name:{
-        type: String,
-        required: true
-    },
     class: {
         type: String,
         required: true
     },
     deviceID: {
+        type: String,
+        required: true
+    },
+    token: {
         type: String,
         required: true
     }
